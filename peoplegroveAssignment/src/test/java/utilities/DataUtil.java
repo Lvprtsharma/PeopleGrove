3b2ce06206f8bccff93522f1a2e3e7b03be3a560
Lovepreet <112539406+sc-lovpre@users.noreply.github.com>
2023-10-12 19:29:19 +0530

    DataUtil Changes

--- peoplegroveAssignment/src/test/java/utilities/DataUtil.java
+++ peoplegroveAssignment/src/test/java/utilities/DataUtil.java
@@ -7,14 +7,14 @@ import base.BaseTest;
 public abstract class DataUtil extends BaseTest {
 	
 	@DataProvider(name = "jobPage")
-	public Object[][] getSignUpData() {
+	public Object[] getSignUpData() {
 
 		return new Object[][] {
             { "Testing Assignment 1", "Total experience 4 years","You'll get an email when * responds."}};
 	}
 
 	@DataProvider(name = "inboxPage")
-	public Object[][] getJobPage() {
+	public Object[] getJobPage() {
 
 		return new Object[][] {
 			{"You reached out to * for help with a job!" }};
